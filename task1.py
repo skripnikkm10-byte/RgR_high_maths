8a6f5c254c44474d3d53d474e53c96002539978d
kirilexer <skripnik.km10@gmail.com>
2025-11-20 19:49:14 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,3 @@
-
 def matrx_creation(p):
     print('\n введите матрицу коэфицентов кватратной матрицы 3x3, построчно.')
     matrixA=([0, 0, 0],[0, 0, 0],[0, 0, 0])
@@ -32,60 +31,40 @@ def var_creation():
 
 def det3x3(matrix):
     x=[]
-    for j in range (3):
-        x.append(1)
-    for j in range (3):
-        x.append(-1)
-    for i in range (3):
-        for j in range (3):
-            if i==j:
-                x[0]=x[0]*matrix[i][j]
-            if i==j+1 or i==j-2:
-                x[1]=x[1]*matrix[i][j]
-            if i==j+2 or i==j-1:
-                x[2]=x[2]*matrix[i][j]
-            if i==j+2 or i==j-2 or (i==1 and j==1):
-                x[3]=x[3]*matrix[i][j]
-            if (i==0 and j==0) or (i==2 and j==1) or (i==1 and j==2):
-                x[4]=x[4]*matrix[i][j]
-            if (i==1 and j==0) or (i==0 and j==1) or (i==2 and j==2):
-                x[5]=x[5]*matrix[i][j]
+    x.append(1*matrix[0][0]*matrix[1][1]*matrix[2][2])
+    x.append(1*matrix[0][1]*matrix[1][2]*matrix[2][0])
+    x.append(1*matrix[0][2]*matrix[1][0]*matrix[2][1])
+    x.append(-1*matrix[0][2]*matrix[1][1]*matrix[2][0])
+    x.append(-1*matrix[0][0]*matrix[1][2]*matrix[2][1])
+    x.append(-1*matrix[0][1]*matrix[1][0]*matrix[2][2])
     return sum(x)
 
-def det2x2(matrix): #TODO
+def det2x2(matrix):
     x=[]
-    for i in range (2):
-        for j in range (2):
-            if i==j and len(x)==0:
-                x.append(matrix[i][j])
-            elif i==j:
-                x[0]=x[0]*matrix[i][j]
-            if (i==j+1 or i==j-1) and len(x)==1:
-                x.append(matrix[i][j])
-            elif i==j+2 or i==j-2:
-                x[1]=x[1]*matrix[i][j]
-    x[1]=x[1]*-1
+    x.append(1*matrix[0][0]*matrix[1][1])
+    x.append(-1*matrix[0][1]*matrix[1][0])
     return sum(x)
+
 def get_minor(matrix, row, col):
     minor=[[0,0],[0,0]]
     k=0
-    x=0
     for i in range(3):
+        if i == row:
+            continue
+        x=0
         for j in range(3):
-            if i!=row and j!=col:
-                minor[k][x]=matrix[i][j]
-                if x<k:
-                    x-=1
-                    k+=1
-                else:
-                    x+=1
-        return minor
+            if j == col:
+                continue
+            minor[k][x]=matrix[i][j]
+            x += 1
+        k += 1
+    return minor
 
 def minor_det_matrix(matrix):
     x=[[0,0,0],[0,0,0],[0,0,0]]
     for i in range(3):
         for j in range(3):
-            x[i][j]=matrix[i][j]*det2x2(get_minor(matrix, i, j))
+            x[i][j]=(-1)**(i+j)*det2x2(get_minor(matrix, i, j))
     return x
 
 def matrix_transposition(matrix):
@@ -97,7 +76,6 @@ def matrix_transposition(matrix):
 
 def inverse_matrix(matrix):
     det = det3x3(matrix)
-    in_matrix = []
     if det==0:
         return 'Обратной матрицы не существует.'
     else:
@@ -107,26 +85,60 @@ def inverse_matrix(matrix):
                 in_matrix[i][j]=in_matrix[i][j]/det
         return in_matrix
 
+def inverse_matrix_for_print(matrix):
+    det = det3x3(matrix)
+    if det==0:
+        return 'Обратной матрицы не существует.'
+    else:
+        in_matrix=matrix_transposition(minor_det_matrix(matrix))
+        for i in range(3):
+            for j in range(3):
+                in_matrix[i][j]=f'{in_matrix[i][j]}/{det}'
+        return in_matrix
+
 def Kramer(matrixA, matrixB):
-    detx=[]
-    ch=0
-    ans=[]
-    matrix=matrixA
-    det=det3x3(matrixA)
+    detx = []
+    ch = 0
+    ans = []
+    matrix = [[matrixA[i][j] for j in range(3)] for i in range(3)]
+    det = det3x3(matrixA)
     for k in range(3):
         for i in range(3):
-            matrix[i][k]=matrixB[i][0]
-            detx.append(det3x3(matrix))
-        matrix=matrixA
+            for j in range(3):
+                matrix[i][j] = matrixA[i][j]
+        for i in range(3):
+            matrix[i][k] = matrixB[i][0]
+        detx.append(det3x3(matrix))
     if det == 0:
         for i in range(3):
-            if detx[i]!=0:
-                ch+=1
-        if ch==0:
+            if detx[i] != 0:
+                ch += 1
+        if ch == 0:
             return 'Система не имеет решений (не совместна).'
-        if ch!=0:
+        if ch != 0:
             return 'Система имеет бесконечное колличество решений.'
     else:
         for x in detx:
-            ans.append(x/det)
+            ans.append(x / det)
         return ans
+
+
+def Inverse_matrix_metod(matrixA, matrixB):
+    if isinstance(matrixA, str):
+        return matrixA
+    Ans_Matrix = [[0], [0], [0]]
+    for i in range(3):
+        for j in range(3):
+            Ans_Matrix[i][0] = Ans_Matrix[i][0] + matrixA[i][j] * matrixB[j][0]
+    return Ans_Matrix
+
+if __name__ == '__main__':
+    var=var_creation()
+    matrixA=matrx_creation(var)
+    matrixB=answer_vector_creation()
+    print('det =', det3x3(matrixA))
+    print('A^(-1) =', inverse_matrix_for_print(matrixA))
+    for i in range (3):
+        mas=Kramer(matrixA, matrixB)
+        print(f'x{i+1} =', mas[i])
+    print('answer vector =', Inverse_matrix_metod(inverse_matrix(matrixA), matrixB))
